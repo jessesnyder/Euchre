a71523515d1046ec397c65dd8955c5265572f7d1
Jesse Snyder <jesse@rasikaconsulting.com>
2016-03-06 17:20:29 -0800

    Deck is mutable again. I have a feeling this might change to be able to test playing and bidding strategies.

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -5,6 +5,17 @@ from deck import Deck
 
 class TestDeck(TestCase):
 
+    def test_after_shuffle_top_card_different(self):
+        deck = Deck()
+        top = deck.pop()
+        deck.shuffle()
+        self.assertNotEqual(top, deck.pop())
+
+    def test_next_issues_new_cards(self):
+        deck = Deck()
+        top = deck.pop()
+        self.assertNotEqual(top, deck.pop())
+
     def test_remove(self):
         deck = Deck()
         card = Card(suit="Hearts", position="Queen")
