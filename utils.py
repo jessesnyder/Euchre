94771155a77bc0b6f4e978a955dbc09396039098
Jesse Snyder <jesse@rasikaconsulting.com>
2016-03-06 17:14:54 -0800

    expand responsibilities of group_players_as_teams()

--- utils.py
+++ utils.py
@@ -3,7 +3,21 @@
 
 def group_players_as_teams(players):
     """Group 4 players into two teams"""
-    return ((players[0], players[2]), (players[1], players[3]))
+    players[0].partner = players[2]
+    players[2].partner = players[0]
+    players[1].partner = players[3]
+    players[3].partner = players[1]
+
+    team1 = (players[0], players[2])
+    team2 = (players[1], players[3])
+
+    for player in team1:
+        player.opposingteam = team2
+
+    for player in team2:
+        player.opposingteam = team1
+
+    return (team1, team2)
 
 
 def next_in_rotation(sequence, current_index):
