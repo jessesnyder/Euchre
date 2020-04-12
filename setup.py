153de2c64da67c3835b57d4f3bff3576cca30d23
Jesse Snyder <jsnyder@wesleyan.edu>
2020-04-12 12:36:27 -0700

    Include README as package long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 """Minimal setup file"""
-
+import os
 from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    readme_text = f.read()
 
 setup(
     name='euchre',
     version='0.1.0',
     description='A Euchre game',
+    long_description=readme_text,
 
     author='Colin Ong-Dean',
     author_email='',
