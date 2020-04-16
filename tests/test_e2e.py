64f8760c22b1fc3a13d9761aee752707f0905b10
Jesse Snyder <jsnyder@wesleyan.edu>
2020-04-16 15:56:56 -0700

    Slight test improvement

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -2,6 +2,7 @@ from euchre import Euchre15
 
 
 def test_end_to_end():
-    result = Euchre15.run(have_real_player=False, games=1)
+    num_games = 2
+    result = Euchre15.run(have_real_player=False, games=num_games)
     scores = [result[0].gamescore, result[1].gamescore]
-    assert scores[0] + scores[1] == 1
+    assert scores[0] + scores[1] == num_games
